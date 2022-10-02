626d7405aca8db0e32fe17fd76b8c76d795ae728
Wandersonrp <wandersonrp.49@hotmail.com>
2022-10-02 12:51:10 -0300

    att

--- Frontend/Js/index.js
+++ Frontend/Js/index.js
@@ -1,2 +1,25 @@
+function digitarLetras(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) =>{
+        setTimeout(() => elemento.innerHTML += letra, 60* i);
+    });
+}
+
+const senai = document.querySelector('#senai');
+const dioDotNet = document.querySelector('#dio');
+const fadivale = document.querySelector('#fadivale');
+
+digitarLetras(senai);
+setInterval(() => { digitarLetras(senai)}, 10000);
+
+digitarLetras(dioDotNet);
+setInterval(() => { digitarLetras(dioDotNet)}, 10000);
+
+digitarLetras(fadivale);
+setInterval(() => { digitarLetras(fadivale)}, 10000);
+
+
+
 
 
